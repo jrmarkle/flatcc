e219641fc434525dc5ee68903baa332550a3ec68
Jamie Markle <jamesm@untether.ai>
2020-08-19 10:36:48 -0400

    test serialize/deserialize binary and JSON

---
+++ test/json_fixed/main.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "mystruct_builder.h"
+#include "mystruct_json_parser.h"
+#include "mystruct_json_printer.h"
+#include "mystruct_reader.h"
+
+Vec3Fixed_t make()
+{
+    Vec3Fixed_t s;
+    s.x[0] = 12;
+    s.x[1] = 34;
+    s.x[2] = 56;
+    return s;
+}
+
+void check(Vec3Fixed_t s)
+{
+    if (s.x[0] != 12 || s.x[1] != 34 || s.x[2] != 56)
+    {
+        printf("struct did not match\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main()
+{
+    {
+        const Vec3Fixed_t s = make();
+        check(s);
+    }
+
+    {
+        void *bufferB;
+
+        {
+            Vec3Fixed_t s1 = make();
+
+            flatcc_builder_t builder;
+            flatcc_builder_init(&builder);
+            Vec3Fixed_create_as_root(&builder, s1.x);
+
+            size_t size = flatcc_builder_get_buffer_size(&builder);
+            assert(size > 0);
+
+            void *bufferA = flatcc_builder_finalize_buffer(&builder, &size);
+            assert(NULL != bufferA);
+
+            bufferB = malloc(size);
+            memcpy(bufferB, bufferA, size);
+
+            free(bufferA);
+            flatcc_builder_clear(&builder);
+        }
+
+        {
+            const Vec3Fixed_t *s2 = Vec3Fixed_as_root(bufferB);
+            check(*s2);
+        }
+
+        free(bufferB);
+    }
+
+    {
+        char *jsonFileBuffer;
+        size_t jsonFileSize;
+        FILE *jsonFile;
+
+        {
+            Vec3Fixed_t s1 = make();
+
+            flatcc_builder_t builderA;
+            flatcc_builder_init(&builderA);
+            Vec3Fixed_create_as_root(&builderA, s1.x);
+
+            size_t size = flatcc_builder_get_buffer_size(&builderA);
+            assert(size > 0);
+
+            void *buffer = flatcc_builder_finalize_buffer(&builderA, &size);
+            assert(NULL != buffer);
+
+            flatcc_builder_clear(&builderA);
+
+            jsonFile = open_memstream(&jsonFileBuffer, &jsonFileSize);
+
+            flatcc_json_printer_t printer;
+            {
+                const int err = flatcc_json_printer_init(&printer, jsonFile);
+                assert(0 == err);
+            }
+
+            const int jsonSize = Vec3Fixed_print_json_as_root(&printer, buffer, size, NULL);
+            assert(jsonSize > 0);
+
+            fflush(jsonFile);
+            free(buffer);
+
+            assert(jsonSize == jsonFileSize);
+        }
+
+        printf("size %zu\n", jsonFileSize);
+        printf("%s\n", jsonFileBuffer);
+
+        {
+            flatcc_builder_t builderB;
+            flatcc_builder_init(&builderB);
+
+            flatcc_json_parser_t parser;
+            const int err = mystruct_parse_json(&builderB, &parser, jsonFileBuffer, jsonFileSize, 0);
+            if (0 != err)
+            {
+                printf("mystruct_parse_json returned error: %s\n", flatcc_json_parser_error_string(err));
+                printf("parser.end:       %p\n", parser.end);
+                printf("parser.error_loc: %p\n", parser.error_loc);
+                printf("parser.line: %d\n", parser.line);
+                printf("parser.pos: %d\n", parser.pos);
+                exit(EXIT_FAILURE);
+            }
+
+            size_t size;
+            void *buffer = flatcc_builder_finalize_buffer(&builderB, NULL);
+
+            const Vec3Fixed_t *parsed = Vec3Fixed_as_root(buffer);
+            check(*parsed);
+        }
+
+        fclose(jsonFile);
+    }
+
+    return EXIT_SUCCESS;
+}
